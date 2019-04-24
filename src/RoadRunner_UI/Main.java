5ce67bf4cd7b0dd65812c285a33b460c4bd94a87
Alliance Mbonigaba <alliancemboni@gmail.com>
2019-04-24 21:14:44 +0200

    Working on Part 2

--- src/RoadRunner_UI/Main.java
+++ src/RoadRunner_UI/Main.java
@@ -1,10 +1,12 @@
 package RoadRunner_UI;
 
 import javafx.application.Application;
+import javafx.event.EventHandler;
 import javafx.scene.Scene;
 import javafx.scene.layout.GridPane;
 import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
+import javafx.scene.input.KeyEvent;
 
 public class Main extends Application {
 
@@ -21,27 +23,36 @@ public class Main extends Application {
         UI visuals = new UI("C:\\Users\\ALU STUDENT\\IdeaProjects\\roadRunnerProjectPersonal\\src\\Test Inputs\\sample_test_input_1.txt");
         //visuals.changeImgAt(1, 1, 2);
         //visuals.linkingImage();
-        visuals.moveUp();
-        visuals.moveUp();
-
-
-
-
-
-
-
-
-
-
-
 
 
 
         root.getChildren().add(visuals.pane);
 
+        Scene scene = new Scene(root, 550, 600);
 
 
-        primaryStage.setScene(new Scene(root, 550, 600));
+        // move roan runner upon four directions on keypress
+        scene.setOnKeyPressed(new EventHandler<KeyEvent>() {
+            @Override
+            public void handle(KeyEvent event) {
+                switch (event.getCode()) {
+                    case UP:
+                        visuals.moveUp(); break;
+                    case DOWN:
+                        visuals.moveDown(); break;
+                    case LEFT:
+                        visuals.moveLeft(); break;
+                    case RIGHT:
+                        visuals.moveRight(); break;
+
+                }
+            }
+        });
+
+
+
+
+        primaryStage.setScene(scene);
         primaryStage.show();
 
 
