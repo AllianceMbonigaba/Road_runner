375a8aa8dbca2179218c6a8db68e3c483dc13646
Kevin Sebineza <ksebineza17@alustudent.com>
2019-04-30 16:05:56 +0200

    Added functionality of printing path to file

--- src/RoadRunner_UI/UI.java
+++ src/RoadRunner_UI/UI.java
@@ -4,9 +4,12 @@ import RoadRunner_Logic.AStar;
 import RoadRunner_Logic.Node;
 import RoadRunner_Logic.ReadFile;
 import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.geometry.Insets;
 import javafx.geometry.Pos;
 import javafx.scene.image.ImageView;
+import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.Background;
 import javafx.scene.layout.BackgroundFill;
 import javafx.scene.layout.CornerRadii;
@@ -17,26 +20,48 @@ import javafx.scene.Scene;
 import javafx.scene.image.Image;
 import javafx.scene.control.Button;
 import java.awt.*;
+import java.io.FileWriter;
 import java.lang.reflect.Array;
-import java.util.ArrayList;
-import java.util.Arrays;
+import java.util.*;
 import java.util.List;
+
+import static javafx.application.Application.launch;
 
 
 public class UI extends Application{
 
+    int undocount = 0;
+
+    int checkScore = 0;
+
+    Stack <Integer[]> undoStack = new Stack<>();
+    Stack <Integer []> redoStack = new Stack<>();
+
+
+    public String pathMap = "";
+    public Boolean setANewStart = true;
+
+
+    int[] holdLastPos = new int[1];
     public Button reset = new Button("Reset");
     public Button redo = new Button("Redo");
     public Button undo = new Button("Undo");
     public Button alldirection = new Button("Enable 8 Directions");
+    public Button loadNewMap = new Button("Load New Map");
+    public Button setSart = new Button("Pick Start");
 
     public Button A = new Button("Solve with A*");
     public Button dijksta = new Button("Solve with Dijksta");
     public Button dfs = new Button("Solve with DFS");
 
 
+    // start position and goal position
+    public int [] goalPos = new int[2];
+    public int [] startPos = new int [2];
+
     int[] currentPosRoadRunner = new int[2];
     int[] previousPosRoadRunner = new int[2];
+    public Boolean checkAllDirection = false;
 
     ArrayList<Integer[]> visited = new ArrayList<>();
 
@@ -44,6 +69,8 @@ public class UI extends Application{
     String text;
     int[][] fileArray;
     int[][] originalFileArray;
+    int[][] toHome;
+
 
     GridPane pane = new GridPane();
     Scene scene = new Scene(pane, 500, 500);
@@ -52,12 +79,14 @@ public class UI extends Application{
         text = file.read_file(fileParth);
         fileArray = file.toArray(text);
         originalFileArray = file.toArray(text);
-        linkingImage();
+
+
+        linkingImage(fileArray);
     }
 
 
-
-    /* Image array Kevin path */
+    //
+//     Image array Kevin path
     String[] imageArray = {"file:C:\\Users\\ALU Student 100\\IdeaProjects\\Road_runner_Kevin&Alliance\\src\\Image Files\\road.jpg",
             "file:C:\\Users\\ALU Student 100\\IdeaProjects\\Road_runner_Kevin&Alliance\\src\\Image Files\\boulder.jpg",
             "file:C:\\Users\\ALU Student 100\\IdeaProjects\\Road_runner_Kevin&Alliance\\src\\Image Files\\pothole.jpg",
@@ -72,12 +101,11 @@ public class UI extends Application{
             "",
             "file:C:\\Users\\ALU Student 100\\IdeaProjects\\Road_runner_Kevin&Alliance\\src\\Image Files\\pothole_alt.jpg",
             "file:C:\\Users\\ALU Student 100\\IdeaProjects\\Road_runner_Kevin&Alliance\\src\\Image Files\\explosive_alt.jpg",
-            "file:C:\\Users\\ALU Student 100\\IdeaProjects\\Road_runner_Kevin&Alliance\\src\\Image Files\\pothole_alt.jpg",
             "file:C:\\Users\\ALU Student 100\\IdeaProjects\\Road_runner_Kevin&Alliance\\src\\Image Files\\coyote_alt.jpg",
             "file:C:\\Users\\ALU Student 100\\IdeaProjects\\Road_runner_Kevin&Alliance\\src\\Image Files\\tarred_alt.jpg",
             "file:C:\\Users\\ALU Student 100\\IdeaProjects\\Road_runner_Kevin&Alliance\\src\\Image Files\\gold_alt.jpg"};
 
-    // Image array Alliance's path
+//    // Image array Alliance's path
 //    String[] imageArray = {"file:C:\\Users\\ALU STUDENT\\IdeaProjects\\roadRunnerProjectPersonal\\src\\Image Files\\road.jpg",
 //            "file:C:\\Users\\ALU STUDENT\\IdeaProjects\\roadRunnerProjectPersonal\\src\\Image Files\\boulder.jpg",
 //            "file:C:\\Users\\ALU STUDENT\\IdeaProjects\\roadRunnerProjectPersonal\\src\\Image Files\\pothole.jpg",
@@ -95,7 +123,7 @@ public class UI extends Application{
 //            "file:C:\\Users\\ALU STUDENT\\IdeaProjects\\roadRunnerProjectPersonal\\src\\Image Files\\coyote_alt.jpg",
 //            "file:C:\\Users\\ALU STUDENT\\IdeaProjects\\roadRunnerProjectPersonal\\src\\Image Files\\tarred_alt.jpg",
 //            "file:C:\\Users\\ALU STUDENT\\IdeaProjects\\roadRunnerProjectPersonal\\src\\Image Files\\gold_alt.jpg"
-//            };
+//    };
 
 
 
@@ -103,18 +131,32 @@ public class UI extends Application{
     public static void main(String[] args) {
         launch(args);
     }
+    @Override
+    public void start(Stage primaryStage){}
 
     /** Method linking the images in their respective array position*/
-    public void linkingImage(){
-        for (int i=0; i<fileArray.length; i++){
-            for (int j=0; j<fileArray[i].length; j++){
+    public void linkingImage(int[][] _fileArray){
+        for (int i=0; i<_fileArray.length; i++){
+            for (int j=0; j<_fileArray[i].length; j++){
 
-                int number = fileArray[i][j];
+                int number = _fileArray[i][j];
 
-                // get current position of the rood runner / start image at index 8
+                int newSartRow = i;
+                int newSartColumn = j;
+
+
+                // get Road runner goal position
+                if (number == 9){
+                    goalPos[0] = i; // row
+                    goalPos[1] = j; // column
+
+                }
+
+                // get current position of the road runner / start image at index 8
                 if(number == 8){
 
-
+                    startPos[0] = i;
+                    startPos[1] = j;
                     currentPosRoadRunner[0] = i; // row
 
                     currentPosRoadRunner[1] = j; // column
@@ -122,17 +164,49 @@ public class UI extends Application{
                     Integer[] visitedCell = {currentPosRoadRunner[0], currentPosRoadRunner[1]};
 
                     visited.add(visitedCell);
+                    undoStack.push(visitedCell);
 
                 }
 
                 Image image = new Image(imageArray[number]);
                 ImageView view = new ImageView(image);
                 view.setPreserveRatio(true);
-                view.setFitHeight(100);
-                view.setFitWidth(100);
+                view.setFitHeight(50);
+                view.setFitWidth(50);
+
+                view.setOnMouseClicked(new EventHandler<MouseEvent>() {
+                    @Override
+                    public void handle(MouseEvent event) {
+                        if(setANewStart){
+
+                            // This will execute whenever the image is clicked.
+                            visited.clear();
+                            undoStack.clear();
+                            int imgToReplace = fileArray[newSartRow][newSartColumn];
+
+                            changeImgAt(startPos[0], startPos[1], imgToReplace);
+                            linkingImage(fileArray);
+                            changeImgAt(newSartRow, newSartColumn, 8);
+
+                            linkingImage(fileArray);
+
+
+//                        currentPosRoadRunner[0] = newSartRow;
+//                        currentPosRoadRunner[1] = newSartColumn;
+
+
+                        }
+
+
+
+
+
+                    }
+                });
 
                 pane.setHgap(5);
                 pane.setVgap(5);
+                pane.maxHeight(50);
                 pane.setAlignment(Pos.CENTER);
                 pane.setBackground(new Background(new BackgroundFill(Color.DARKGRAY, CornerRadii.EMPTY, Insets.EMPTY)));
 
@@ -144,21 +218,43 @@ public class UI extends Application{
         }
     }
 
+
+
     public GridPane leftButtons(){
         GridPane buttons = new GridPane();
 
         buttons.setVgap(5);
         buttons.setHgap(5);
-        buttons.setAlignment(Pos.BOTTOM_LEFT);
+
+        buttons.setAlignment(Pos.BOTTOM_CENTER);
+
+        buttons.setMaxHeight(2);
 
         buttons.add(undo, 0, 0);
-        buttons.add(redo, 0, 1);
-        buttons.add(alldirection, 0, 2);
-        buttons.add(reset, 0, 3);
+        buttons.add(redo, 1, 0);
+        buttons.add(alldirection, 2, 0);
+        buttons.add(reset, 3, 0);
+        buttons.add(A, 4, 0);
+        buttons.add(setSart, 5, 0);
+
 
 
         return buttons;
     }
+
+
+
+
+    public void reset(){
+        visited.clear();
+        linkingImage(originalFileArray);
+    }
+
+    public void pickSart(){
+
+
+    }
+
 
 
 
@@ -170,8 +266,8 @@ public class UI extends Application{
 
 
         view.setPreserveRatio(true);
-        view.setFitHeight(100);
-        view.setFitWidth(100);
+        view.setFitHeight(50);
+        view.setFitWidth(50);
 
 
         pane.add(view, column, row);
@@ -180,7 +276,7 @@ public class UI extends Application{
     }
 
     public Boolean isVisited(int row, int column){
-        int[] arr = {row, column};
+
 
         for(int i = 0 ; i < visited.size(); i++){
 
@@ -189,18 +285,22 @@ public class UI extends Application{
             }
         }
 
-       return false;
+        return false;
     }
 
     public void putAltImage(int imgIndexTochange){
         if(imgIndexTochange != 1 && imgIndexTochange != 8 && imgIndexTochange != 9 && imgIndexTochange != 7){
             changeImgAt(currentPosRoadRunner[0], currentPosRoadRunner[1], imgIndexTochange + 10);
-
+            getscore(imgIndexTochange);
         }
         else {
 
             changeImgAt(previousPosRoadRunner[0], previousPosRoadRunner[1], originalFileArray[previousPosRoadRunner[0]][previousPosRoadRunner[1]]);
+
         }
+
+
+
 
     }
 
@@ -218,7 +318,8 @@ public class UI extends Application{
 
             // hold the image index to be changed
             int toBeChanged = originalFileArray[currentPosRoadRunner[0]][currentPosRoadRunner[1]];
-            System.out.println(toBeChanged);
+
+
 
             // put alt image
             putAltImage(toBeChanged);
@@ -227,6 +328,7 @@ public class UI extends Application{
             Integer[] visitedCell = {currentPosRoadRunner[0] - 1, currentPosRoadRunner[1]};
             currentPosRoadRunner[0] = currentPosRoadRunner[0] - 1;
             visited.add(visitedCell);
+            undoStack.push(visitedCell);
 
         }
 
@@ -248,6 +350,7 @@ public class UI extends Application{
             Integer[] visitedCell = {currentPosRoadRunner[0] + 1, currentPosRoadRunner[1]};
             currentPosRoadRunner[0] = currentPosRoadRunner[0] + 1;
             visited.add(visitedCell);
+            undoStack.push(visitedCell);
 
         }
 
@@ -269,6 +372,7 @@ public class UI extends Application{
             Integer[] visitedCell = {currentPosRoadRunner[0], currentPosRoadRunner[1] - 1};
             currentPosRoadRunner[1] = currentPosRoadRunner[1] - 1;
             visited.add(visitedCell);
+            undoStack.push(visitedCell);
 
         }
     }
@@ -279,6 +383,7 @@ public class UI extends Application{
 
 
             // hold the image index to be changed
+
             int toBeChanged = originalFileArray[currentPosRoadRunner[0]][currentPosRoadRunner[1]];
 
 
@@ -289,6 +394,7 @@ public class UI extends Application{
             Integer[] visitedCell = {currentPosRoadRunner[0], currentPosRoadRunner[1] + 1};
             currentPosRoadRunner[1] = currentPosRoadRunner[1] + 1;
             visited.add(visitedCell);
+            undoStack.push(visitedCell);
 
         }
     }
@@ -301,15 +407,17 @@ public class UI extends Application{
             // hold the image index to be changed
             int toBeChanged = originalFileArray[currentPosRoadRunner[0]][currentPosRoadRunner[1]];
 
-
             // put alt image
             putAltImage(toBeChanged);
 
             changeImgAt(currentPosRoadRunner[0] - 1, currentPosRoadRunner[1] - 1, 7);
             Integer[] visitedCell = {currentPosRoadRunner[0] - 1, currentPosRoadRunner[1] - 1};
             currentPosRoadRunner[1] = currentPosRoadRunner[1] - 1;
-            currentPosRoadRunner[0] = currentPosRoadRunner[1] - 1;
+
+            currentPosRoadRunner[0] = currentPosRoadRunner[0] - 1;
+
             visited.add(visitedCell);
+            undoStack.push(visitedCell);
 
         }
     }
@@ -322,15 +430,15 @@ public class UI extends Application{
             // hold the image index to be changed
             int toBeChanged = originalFileArray[currentPosRoadRunner[0]][currentPosRoadRunner[1]];
 
-
             // put alt image
             putAltImage(toBeChanged);
 
             changeImgAt(currentPosRoadRunner[0] - 1, currentPosRoadRunner[1] + 1, 7);
             Integer[] visitedCell = {currentPosRoadRunner[0] - 1, currentPosRoadRunner[1] + 1};
             currentPosRoadRunner[1] = currentPosRoadRunner[1] + 1;
-            currentPosRoadRunner[0] = currentPosRoadRunner[1] - 1;
+            currentPosRoadRunner[0] = currentPosRoadRunner[0] - 1;
             visited.add(visitedCell);
+            undoStack.push(visitedCell);
 
         }
 
@@ -346,19 +454,71 @@ public class UI extends Application{
             // hold the image index to be changed
             int toBeChanged = originalFileArray[currentPosRoadRunner[0]][currentPosRoadRunner[1]];
 
-
             // put alt image
             putAltImage(toBeChanged);
 
             changeImgAt(currentPosRoadRunner[0] + 1, currentPosRoadRunner[1] - 1, 7);
             Integer[] visitedCell = {currentPosRoadRunner[0] + 1, currentPosRoadRunner[1] - 1};
 
-            currentPosRoadRunner[0] = currentPosRoadRunner[1] + 1;
+            currentPosRoadRunner[0] = currentPosRoadRunner[0] + 1;
             currentPosRoadRunner[1] = currentPosRoadRunner[1] - 1;
 
             visited.add(visitedCell);
+            undoStack.push(visitedCell);
 
         }
+
+    }
+
+    // redo the last action
+    public void redo(){
+
+    }
+
+
+    // setting new start
+    public  void setNewStart(){
+
+
+    }
+
+    // loading a new map
+    public void loadNewMap(){
+        System.out.println("Copy and past path for the new map on the next line; ");
+        Scanner path = new Scanner(System.in);
+        String pathValue = path.nextLine();
+
+        text = file.read_file(pathValue);
+        fileArray = file.toArray(text);
+        originalFileArray = file.toArray(text);
+
+        linkingImage(fileArray);
+    }
+
+
+    public void undo(){
+
+        if((!undoStack.isEmpty()) && undocount < 3 ){
+
+            int orgImg = originalFileArray[currentPosRoadRunner[0]][currentPosRoadRunner[1]];
+            changeImgAt(currentPosRoadRunner[0], currentPosRoadRunner[1], orgImg);
+            Integer[] toPop = undoStack.pop();
+
+            visited.remove(toPop);
+
+            redoStack.push(toPop);
+
+            //Integer[] toMoveTo = undoStack.pop();
+            changeImgAt(toPop[0], toPop[1], 7);
+            currentPosRoadRunner[0] = toPop[0];
+            currentPosRoadRunner[1] = toPop[1];
+            undocount++;
+            if(undocount == 3){
+                undocount = 0;
+            }
+
+        }
+
 
     }
 
@@ -378,26 +538,31 @@ public class UI extends Application{
             changeImgAt(currentPosRoadRunner[0] + 1, currentPosRoadRunner[1] + 1, 7);
             Integer[] visitedCell = {currentPosRoadRunner[0] + 1, currentPosRoadRunner[1] + 1};
 
-            currentPosRoadRunner[0] = currentPosRoadRunner[1] + 1;
+            currentPosRoadRunner[0] = currentPosRoadRunner[0] + 1;
             currentPosRoadRunner[1] = currentPosRoadRunner[1] + 1;
 
             visited.add(visitedCell);
+            undoStack.push(visitedCell);
 
         }
 
     }
 
-
     /** Method that returns an array containing paths created by a* algorithm
      * Time Complexity 0(E) where E is number of edges traversed*/
 
-    public ArrayList<String> astarPath(Node start, Node end){
+    public ArrayList<String> astarPath(Node start, Node end) {
+        String message= "Start: " + start.getRow() + "," + start.getCol() + "\n";
+        message += "End: " +  end.getRow() + "," + end.getCol() + "\n";
         int rows = file.rowSize;
         int columns = file.columnSize;
-        int[][]blocks = new int[][]{{0,0}};
+//        int[][]blocks = new int[][]{{14,14}};
         ArrayList<String> path = new ArrayList<>();
         AStar astar = new AStar(rows, columns, start, end);
-        astar.setBlocks(blocks);
+        if(checkAllDirection){
+            astar.allowed8Directions = true;
+        }
+//        astar.setBlocks(blocks);
         List<Node> findpath = astar.findPath();
 
         int StartedRow = start.getRow();
@@ -411,42 +576,78 @@ public class UI extends Application{
 
             if (rowDifference == 1 && colDifference == 0){
                 path.add("North");
+                message += "North\n";
+                moveUp();
             }
             else if (rowDifference == 0 && colDifference == 1){
                 path.add("West");
+                message += "West\n";
+                moveLeft();
             }
             else if (rowDifference == -1 && colDifference == 0){
                 path.add("South");
+                message += "South\n";
+                moveDown();
             }
             else if (rowDifference == 0 && colDifference == -1){
                 path.add("East");
+                message += "East\n";
+                moveRight();
             }
             else if (rowDifference == 1 && colDifference == 1){
                 path.add("NorthWest");
+                message += "NorthWest\n";
+                moveNW();
             }
             else if (rowDifference == 1 && colDifference == -1){
                 path.add("NorthEast");
+                message += "NorthEast\n";
+                moveNE();
             }
             else if (rowDifference == -1 && colDifference == 1){
                 path.add("SouthWest");
+                message += "SouthWest\n";
+                moveSW();
             }
             else if (rowDifference == -1 && colDifference == -1){
                 path.add("SouthEast");
+                message += "SouthEast\n";
+                moveSE();
             }
 
             StartedRow = movedRow;
             StartedCol = movedCol;
         }
+        try{
+        FileWriter file = new FileWriter("C:\\Users\\ALU Student 100\\IdeaProjects\\Road_runner_Kevin&Alliance\\src\\Test Inputs\\AStarFile");
+        file.write(message);
+        file.close();
 
+    }catch (Exception e){
+            System.out.println("File not found");
+        }
         return path;
     }
 
 
+    public void getscore(int number){
+        HashMap<Integer, Integer> score = new HashMap<>();
+        score.put(0, -1);
+        score.put(2, -2);
+        score.put(3, -4);
+        score.put(4, -8);
+        score.put(5, 1);
+        score.put(6, 5);
 
-    /** Creating the stage*/
-    @Override
-    public void start(Stage primaryStage) {
+        checkScore += score.get(number);
 
     }
+
+
+
+
+
+    /** Creating the stage*/
+
 
 }
